c2d2c1c729ac674a48f9a0786432eb392fd26ed9
Jimbo-Farmer <74548900+Jimbo-Farmer@users.noreply.github.com>
2021-03-26 10:44:26 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -12,3 +12,74 @@ const cats = [
     }
 ];
 
+
+// Question 1
+
+const cat = {
+        complain: function() {
+            console.log("Meow!");
+        }
+};
+
+
+// Question 2 
+
+const heading = document.querySelector("h3");
+heading.innerHTML = "Updated Heading";
+
+
+// Question 3
+
+heading.style.fontSize = "2em";
+
+
+// Question 4
+
+heading.classList.add("subheading");
+
+
+// Question 5
+
+const paragraphs = document.querySelectorAll("p");
+for(let i = 0; i < paragraphs.length; i++){
+    paragraphs[i].style.color = "red";
+}
+
+
+// Question 6
+
+const resultsContainer = document.querySelector(".results");
+resultsContainer.innerHTML = "<p>New paragraph</p>";
+resultsContainer.style.backgroundColor = "yellow";
+
+
+// Question 7
+
+function logger(list){
+    for(let i= 0; i< list.length; i++){
+        console.log(list[i].name);
+    }
+}
+
+logger(cats);
+
+
+// Question 8
+
+function createCats(cats){
+    let newHTML = ``;
+    for(let i = 0; i < cats.length; i++){
+        let ageProperty = "Age unknown";
+        if(cats[i].age){
+            ageProperty = cats[i].age;
+        }
+        newHTML += `<div>
+        <h5>${cats[i].name}</h5>
+        <p>${ageProperty}</p>
+        </div>`
+    }
+    return newHTML;
+}
+
+const catContainer = document.querySelector(".cat-container");
+catContainer.innerHTML = createCats(cats);
